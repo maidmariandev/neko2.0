198c07a41af39adea5024d719cdc5c63ae776f24
runacode <59107019+runacode@users.noreply.github.com>
2020-08-16 19:06:09 -0500

    fixed docker

--- client/src/components/header.vue
+++ client/src/components/header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
     <div class="neko">
-      <img src="@/assets/images/logo.svg" alt="n.eko" />
-      <span><b>n</b>.eko</span>
+
+      <span>Bellee Poke</span>
     </div>
     <ul class="menu">
       <li>
